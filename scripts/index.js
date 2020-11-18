ae7bc78557b66e86fe560640f2186d7a7104b835
Mateus Araújo <mateusaraujo996@gmail.com>
2020-11-18 18:59:43 -0300

    Ajustes na integração da pagina Anuncio

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,10 @@ const imgSearch = document.querySelector(".img-home");
 const sectionSearch = document.querySelector(".search");
 const sectionResult = document.querySelector(".result");
 const footer = document.querySelector(".footer");
+const cardsArea = document.querySelector("#cards-area");
+const inputCep = document.querySelector("#input_cep");
+
+const anuncio = {};
 
 if(sessionStorage.estadoHome == "visitado"){
     reduzIndex();
@@ -13,6 +17,7 @@ if(sessionStorage.estadoHome == "visitado"){
 btnBuscar.addEventListener("click", () => {
     sessionStorage.setItem("estadoHome","visitado");
     reduzIndex();
+    buscaAnimais();
 })
 
 function reduzIndex(){
@@ -27,3 +32,25 @@ function ampliaIndex(){
     imgSearch.classList.remove("display-none")
 }
 
+function buscaAnimais(){
+    cardsArea.innerHTML = ``
+    fetch(`http://localhost:8080/anuncio/busca?cep=${inputCep.value}`)
+        .then(res => res.json())
+        .then(anuncio => {
+            for(anuncioRecebido of anuncio.content){
+                cardsArea.innerHTML += `<a href="./pages/petprofile.html" class="res-card">
+                <div class="res-card-img">
+                    <img src="../KdMeuBichinho-BackEnd/${anuncioRecebido.idAnimal.fotos.caminho}" alt="">
+                </div>
+                <div class="res-card-txt">
+                    <p>${anuncioRecebido.idAnimal.nome}</p>
+                    <p>${anuncioRecebido.idAnimal.nome}</p>
+                </div>
+                <div class="res-card-tag">
+                    <span class="tag">${anuncioRecebido.idCategoria.classificacao}</span>    
+                </div>
+            </a>`
+            }
+            console.log(anuncio)
+        })
+}
